cbea16e44db0e9dce2580e3dfc8811fdf9212de1
yvonne <zhangyf-o@glodon.com>
2022-04-30 23:34:55 +0800

    angular learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+// import module
+import { ElModule } from 'element-angular'
+
+// if you use webpack, import style
+import 'element-angular/theme/index.css';
+import {MatTableModule} from '@angular/material/table';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { LayoutComponent } from './layout/layout.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { CommonTableComponent } from './common-table/common-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    LeftContainerComponent,
+    CommonTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatSliderModule,
+    ElModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
