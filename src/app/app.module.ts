ddac46d26183570b8bac712627b52988389bb89b
yvonne <zhangyf-o@glodon.com>
2022-05-01 12:13:36 +0800

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,27 +11,41 @@ import { ElModule } from 'element-angular'
 import 'element-angular/theme/index.css';
 import {MatTableModule} from '@angular/material/table';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { LayoutComponent } from './layout/layout.component';
 import { LeftContainerComponent } from './left-container/left-container.component';
 import { CommonTableComponent } from './common-table/common-table.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuListComponent } from './menu-list/menu-list.component'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { TopContainerComponent } from './top-container/top-container.component';
+import {MatBadgeModule} from '@angular/material/badge';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LayoutComponent,
     LeftContainerComponent,
-    CommonTableComponent
+    CommonTableComponent,
+    MenuListComponent,
+    TopContainerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
     MatSliderModule,
+    MatMenuModule,
+    MatIconModule,
     ElModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatBadgeModule
+    // MatCardHarness
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-left-container',
   templateUrl: './left-container.component.html',
-  styleUrls: ['./left-container.component.less']
+  styleUrls: ['./left-container.component.less'],
 })
+
 export class LeftContainerComponent implements OnInit {
 
   constructor() { }
+  hidden = false;
 
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
   ngOnInit(): void {
   }
 
